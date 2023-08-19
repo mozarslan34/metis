55eff9fe79aaffe71c7f8b81348fc9e80461582e
DincEmre <emre.dinc@consultant.turkcell.com.tr>
2023-08-19 23:03:51 +0300

    springboot first commit

---
+++ src/main/java/com/edu/metis/MetisApplication.java
@@ -0,0 +1,13 @@
+package com.edu.metis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/metis/MetisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.metis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
