c08ccbf53de5947487ec98bf41c61faf2fc9e11b
MERTCAN OZARSLAN <mertcan.ozarslan@consultant.turkcell.com.tr>
2023-08-24 01:29:33 +0300

    base structure done

--- src/main/java/Deneme.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Emre Dinç
- */
-public class Deneme {
-    // boş adam mertcan için deneme commiti
-}
---
+++ src/main/java/com/edu/metis/controller/DenemeController.java
@@ -0,0 +1,29 @@
+package com.edu.metis.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Mertcan Özarslan
+ */
+
+@RestController
+@RequestMapping("/deneme")
+@RequiredArgsConstructor
+public class DenemeController {
+
+    @GetMapping
+    public ResponseEntity<String> init(){
+        return ResponseEntity.ok("hello metis");
+    }
+
+    @PostConstruct
+    public void sayHello(){
+        System.out.println("hello metis");
+    }
+}
